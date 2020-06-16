76f0cdc3b11ca20fe3eda1753445f7751ffc4643
roylaw <roylaw.w@gmail.com>
2020-06-16 02:06:15 -0400

    initial commit

---
+++ swift-simple-youtube-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-simple-youtube-clone
+//
+//  Created by Weibin Luo on 2020-06-16.
+//  Copyright © 2020 Weibin Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
